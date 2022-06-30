6da75c906de9048e1a241760fedfec439c2731a2
bodyMist <sky834459@gmail.com>
2022-07-01 06:19:35 +0900

    test commit

--- src/api/api.js
+++ src/api/api.js
@@ -3,50 +3,48 @@ import api from "../key";
 
 // enroll
 const getBooksList = () => {
-  return axios.get(`${api.url}/books`);
+  return axios.get(`${api}/books`);
 };
 
 const deleteBook = ({ bookId }) => {
-  return axios.delete(`${api.url}/admin/books/${bookId}`);
+  return axios.delete(`${api}/admin/books/${bookId}`);
 };
 
 const addNewBooks = ({ data }) => {
-  return axios.post(`${api.url}/admin/books`, data);
+  return axios.post(`${api}/admin/books`, data);
 };
 
 const modifyBook = ({ bookId, data }) => {
-  return axios.put(`${api.url}/admin/books/${bookId}`, data);
+  return axios.put(`${api}/admin/books/${bookId}`, data);
 };
 
 // bookList
 const getBookListapi = async (bookId) => {
-  return await axios.get(`${api.url}/books/${bookId}/stocks`);
+  return await axios.get(`${api}/books/${bookId}/stocks`);
 };
 
 const addBookListapi = async (bookId, body) => {
-  return await axios.post(`${api.url}/admin/books/${bookId}/Stocks`, body);
+  return await axios.post(`${api}/admin/books/${bookId}/Stocks`, body);
 };
 
 const delBookListapi = async (bookId, delBookId, body) => {
   return await axios.delete(
-    `${api.url}/admin/books/${bookId}/stocks/${delBookId}`,
+    `${api}/admin/books/${bookId}/stocks/${delBookId}`,
     body
   );
 };
 
 // RsvList
 const getUserListapi = async (bookId) => {
-  return await axios.get(`${api.url}/books/${bookId}/reservations`);
+  return await axios.get(`${api}/books/${bookId}/reservations`);
 };
 
 const modiUsersListapi = async (id, body) => {
-  return await axios.put(`${api.url}/admin/reservations/${id}`, body);
+  return await axios.put(`${api}/admin/reservations/${id}`, body);
 };
 
 const cancelUsersListapi = async (bookId, id) => {
-  return await axios.delete(
-    `${api.url}/admin/books/${bookId}/reservations/${id}`
-  );
+  return await axios.delete(`${api}/admin/books/${bookId}/reservations/${id}`);
 };
 
 export {
