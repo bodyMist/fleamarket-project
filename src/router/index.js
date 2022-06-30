70e1c8cee2f15418b887045ccc708ae4587f6008
bodyMist <sky834459@gmail.com>
2022-07-01 06:26:15 +0900

    feat : delete unnecessary router path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
 import Enroll from "../views/Enroll.vue";
 
 Vue.use(VueRouter);
@@ -8,21 +8,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/Manager",
-    name: "Manager",
+    name: "Enroll",
     component: Enroll,
   },
   {
